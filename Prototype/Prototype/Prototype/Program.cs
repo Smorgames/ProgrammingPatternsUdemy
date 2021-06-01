aaed9011eefe15d2b794da806dfdcb7255b4acb6
Zaveryukha_Ivan <IZaveryukha@croc.ru>
2021-06-01 12:29:18 +0300

    Solution of Builder, Factory, Prototype and Singleton tasks

---
+++ Prototype/Prototype/Prototype/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Prototype
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line = new Line(new Point(1, 6), new Point(4, 3));
+            var lineCopy = line.DeepCopy();
+            lineCopy.Start = new Point(888, 999);
+            lineCopy.End = new Point(357, 1999);
+
+            Console.WriteLine(line);
+            Console.WriteLine(lineCopy);
+        }
+    }
+
+    public class Point
+    {
+        public int X, Y;
+
+        public Point(Point point)
+        {
+            X = point.X;
+            Y = point.Y;
+        }
+
+        public Point(int x, int y)
+        {
+            X = x;
+            Y = y;
+        }
+
+        public override string ToString() => $"point({X}; {Y})";
+    }
+
+    public class Line
+    {
+        public Point Start, End;
+
+        public Line(Line line)
+        {
+            Start = new Point(line.Start);
+            End = new Point(line.End);
+        }
+
+        public Line(Point start, Point end)
+        {
+            Start = start;
+            End = end;
+        }
+
+        public Line DeepCopy()
+        {
+            var copy = new Line(this);
+            return copy;
+        }
+
+        public override string ToString() => $"Line with start {Start.ToString()} and end {End.ToString()}";
+    }
+}
---
+++ Singleton/Singleton/Singleton/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<object> factoryMethod = ObjectFactory.GetNotSingletonObject;
+            Console.WriteLine(SingletonTester.IsSingleton(factoryMethod));
+
+            factoryMethod = ObjectFactory.GetSingletonObject;
+            Console.WriteLine(SingletonTester.IsSingleton(factoryMethod));
+        }
+    }
+
+    public class SingletonTester
+    {
+        public static bool IsSingleton(Func<object> func)
+        {
+            object obj1 = func();
+            object obj2 = func();
+            return object.Equals(obj1, obj2);
+        }
+    }
+
+    public class SingletonObject
+    {
+        private static Lazy<SingletonObject> _instance = new Lazy<SingletonObject>(() => new SingletonObject());
+
+        public static SingletonObject Instance => _instance.Value;
+    }
+
+    public class ObjectFactory
+    {
+        public static object GetNotSingletonObject()
+        {
+            object obj = new object();
+            return obj;
+        }
+
+        public static object GetSingletonObject()
+        {
+            SingletonObject singleton = SingletonObject.Instance;
+            return singleton;
+        }
+    }
+}
