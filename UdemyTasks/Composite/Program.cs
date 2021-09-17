72811f33af86ba2e975336eb4fe16a9fe7a1c172
Zaveryukha_Ivan <IZaveryukha@croc.ru>
2021-09-17 17:20:00 +0300

    WIP: Composite problem

--- UdemyTasks/Composite/Program.cs
+++ UdemyTasks/Composite/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Composite
 {
@@ -6,7 +9,67 @@ namespace Composite
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             var sv = new SingleValue() { i = 4 };
+             var mv = new ManyValues();
+             
+             mv.Add(4);
+             mv.Add(3);
+             mv.Add(2);
+             mv.Add(1);
+             mv.Add(5);
+            
+             Console.WriteLine(sv.Sum());
+             Console.WriteLine(mv.Sum());
+        }
+    }
+    
+    public interface IValueContainer : IEnumerable<int>
+    {
+        public int i { get; set; }
+    }
+
+    public class SingleValue : IValueContainer
+    {
+        public int Value;
+        public int i { get; set; }
+
+        public IEnumerator<int> GetEnumerator()
+        {
+            yield return i;
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+
+    public class ManyValues : List<int>, IValueContainer
+    {
+        public int i { get; set; }
+    }
+
+    public class Test
+    {
+        
+    }
+
+    public class Test2 : List<int>
+    {
+        
+    }
+
+    public static class ExtensionMethods
+    {
+        public static int Sum(this List<IValueContainer> containers)
+        {
+            int result = 0;
+            
+            foreach (var c in containers)
+                foreach (var i in c)
+                    result += i;
+            
+            return result;
         }
     }
 }
