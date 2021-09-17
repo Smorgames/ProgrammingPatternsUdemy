8838c5427251cea397053b484a205846056a5213
Zaveryukha_Ivan <IZaveryukha@croc.ru>
2021-09-17 12:54:31 +0300

    Solved Builder problem

---
+++ UdemyTasks/Builder/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+
+namespace Builder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var cb = new CodeBuilder("Person").AddField("Name", "string").AddField("Age", "int");
+            Console.WriteLine(cb);
+        }
+    }
+
+    public class CodeBuilder
+    {
+        private string _root;
+        private StringBuilder _stringBuilder;
+
+        private int _indentSize = 2;
+
+        public CodeBuilder(string root)
+        {
+            _root = root;
+            _stringBuilder = new StringBuilder();
+            _stringBuilder.AppendLine($"public class {_root}");
+            _stringBuilder.AppendLine("{");
+        }
+
+        public CodeBuilder AddField(string varName, string varType)
+        {
+            _stringBuilder.AppendLine(new string(' ', _indentSize) + $"public {varType} {varName};");
+            return this;
+        }
+
+        public override string ToString()
+        {
+            _stringBuilder.AppendLine("}");
+            return _stringBuilder.ToString();
+        }
+    }
+}
