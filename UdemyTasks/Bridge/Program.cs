c54d62971457ab5afe5f1dfa462b31d8dd44a6a1
Zaveryukha_Ivan <IZaveryukha@croc.ru>
2021-09-17 16:33:16 +0300

    Solved Bridge problem

--- UdemyTasks/Bridge/Program.cs
+++ UdemyTasks/Bridge/Program.cs
@@ -6,7 +6,61 @@ namespace Bridge
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var vectorTriangle = new Triangle(new VectorRenderer());
+            var pixelTriangle = new Triangle(new RasterRenderer());
+            var vectorSquare = new Square(new VectorRenderer());
+            var pixelSquare = new Square(new RasterRenderer());
+
+            Console.WriteLine(vectorTriangle.ToString());
+            Console.WriteLine(pixelTriangle.ToString());
+            Console.WriteLine(vectorSquare.ToString());
+            Console.WriteLine(pixelSquare.ToString());
         }
+    }
+
+    public abstract class Shape
+    {
+        public string Name { get; set; }
+
+        protected IRenderer _iRenderer;
+        
+        public Shape(IRenderer iRenderer)
+        {
+            _iRenderer = iRenderer;
+        }
+        
+        public override string ToString() => $"Drawing {Name} {_iRenderer.WhatToRenderAs}";
+
+    }
+
+    public class Square : Shape
+    {
+        public Square(IRenderer iRenderer) : base(iRenderer)
+        {
+            Name = "Square";
+        }
+    }
+    
+    public class Triangle : Shape
+    {
+        public Triangle(IRenderer iRenderer) : base(iRenderer)
+        {
+            Name = "Triangle";
+        }
+    }
+
+    public interface IRenderer
+    {
+        string WhatToRenderAs { get; }
+    }
+
+    public class VectorRenderer : IRenderer
+    {
+        public string WhatToRenderAs => "as lines";
+    }
+    
+    public class RasterRenderer : IRenderer
+    {
+        public string WhatToRenderAs => "as pixels";
     }
 }
