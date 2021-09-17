b3cd89a7be095b71f7932e47e752caa30217b16d
Zaveryukha_Ivan <IZaveryukha@croc.ru>
2021-09-17 16:09:46 +0300

    Solved Adapter problem

--- UdemyTasks/Adapter/Program.cs
+++ UdemyTasks/Adapter/Program.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace Adapter
 {
@@ -7,7 +6,38 @@ namespace Adapter
     {
         static void Main(string[] args)
         {
+            var square = new Square() { Side = 5 };
+            var adapter = new SquareToRectangleAdapter(square);
+            Console.WriteLine(adapter.Area());
+        }
+    }
+    
+    public class Square
+    {
+        public int Side;
+    }
 
+    public interface IRectangle
+    {
+        int Width { get; }
+        int Height { get; }
+    }
+    
+    public static class ExtensionMethods
+    {
+        public static int Area(this IRectangle rc)
+        {
+            return rc.Width * rc.Height;
+        }
+    }
+
+    public class SquareToRectangleAdapter : IRectangle
+    {
+        public int Width { get; }
+        public int Height { get; }
+        public SquareToRectangleAdapter(Square square)
+        {
+            Width = Height = square.Side;
         }
     }
 }
