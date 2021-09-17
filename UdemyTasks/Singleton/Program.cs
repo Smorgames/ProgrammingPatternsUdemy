e802b7475193bd7daa281e73ac2a4114666fae2b
Zaveryukha_Ivan <IZaveryukha@croc.ru>
2021-09-17 16:01:08 +0300

    Solved Singleton problem

---
+++ UdemyTasks/Singleton/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<object> func = SingletonFactory.GetSingletonObject;
+            Func<object> func2 = SingletonFactory.GetNotSingletonObject;
+            
+            Console.WriteLine(IsSingleton(func));
+            Console.WriteLine(IsSingleton(func2));
+        }
+        
+        public static bool IsSingleton(Func<object> func)
+        {
+            var obj1 = func.Invoke();
+            var obj2 = func.Invoke();
+            return obj1 == obj2;
+        }
+    }
+    
+    public class SingletonObject
+    {
+        private static Lazy<SingletonObject> _instance =
+            new Lazy<SingletonObject>(() => new SingletonObject());
+
+        public static SingletonObject Instance => _instance.Value;
+    }
+
+    public class SingletonFactory
+    {
+        public static SingletonObject GetSingletonObject()
+        {
+            var obj = SingletonObject.Instance;
+            return obj;
+        }
+
+        public static object GetNotSingletonObject()
+        {
+            var obj = new object();
+            return obj;
+        }
+    }
+}
