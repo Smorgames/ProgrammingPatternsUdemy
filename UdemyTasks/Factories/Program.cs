0ceb85dd297632685c0753ec2cdbd8d576988b6d
Zaveryukha_Ivan <IZaveryukha@croc.ru>
2021-09-17 14:05:00 +0300

    Solved Factories problem

---
+++ UdemyTasks/Factories/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Factories
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var personFactory = new PersonFactory();
+            var person1 = personFactory.CreatePerson("Ivan"); Console.WriteLine(person1);
+            var person2 = personFactory.CreatePerson("John"); Console.WriteLine(person2);
+        }
+    }
+
+    public class Person
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public override string ToString()
+        {
+            return $"Person with name {Name} has ID = {Id}";
+        }
+    }
+
+    public class PersonFactory
+    {
+        private static int _personCounter = 0;
+
+        public Person CreatePerson(string name)
+        {
+            var person = new Person() { Name = name, Id = _personCounter++ };
+            return person;
+        }
+    }
+}
