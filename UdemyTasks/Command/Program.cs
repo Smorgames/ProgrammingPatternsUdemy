23c8bedfca47bef1d0df91a3a34c57a2d0b0a5cc
Zaveryuha_Ivan <IZaveryukha@croc.ru>
2021-09-22 22:45:17 +0300

    Solved Command problem

--- UdemyTasks/Command/Program.cs
+++ UdemyTasks/Command/Program.cs
@@ -9,4 +9,41 @@ namespace Command
             Console.WriteLine("Hello World!");
         }
     }
+
+    public class Command
+    {
+        public enum Action
+        {
+            Deposit,
+            Withdraw
+        }
+
+        public Action TheAction;
+        public int Amount;
+        public bool Success;
+    }
+
+    public class Account
+    {
+        public int Balance { get; set; }
+
+        public void Process(Command c)
+        {
+            if (c.TheAction == Command.Action.Deposit)
+            {
+                Balance += c.Amount;
+                c.Success = true;
+            }
+            if (c.TheAction == Command.Action.Withdraw)
+            {
+                if (c.Amount <= Balance)
+                {
+                    Balance -= c.Amount;
+                    c.Success = true;
+                }
+                else
+                    c.Success = false;
+            }
+        }
+    }
 }
