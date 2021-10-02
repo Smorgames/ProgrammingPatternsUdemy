37ebb466c037ef82650b68673cf6279323d41242
Zaveryuha_Ivan <IZaveryukha@croc.ru>
2021-10-02 16:29:17 +0300

    Solved NullObject problem

--- UdemyTasks/NullObject/Program.cs
+++ UdemyTasks/NullObject/Program.cs
@@ -6,7 +6,71 @@ namespace NullObject
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var nullLog = new NullLog();
+            var a = new Account(nullLog);
+
+            a.SomeOperation();
+            a.SomeOperation();
+            a.SomeOperation();
+            a.SomeOperation();
+            a.SomeOperation();
+            a.SomeOperation();
         }
+    }
+
+    public interface ILog
+    {
+        // maximum # of elements in the log
+        int RecordLimit { get; }
+
+        // number of elements already in the log
+        int RecordCount { get; set; }
+
+        // expected to increment RecordCount
+        void LogInfo(string message);
+    }
+
+    public class Account
+    {
+        private ILog log;
+
+        public Account(ILog log)
+        {
+            this.log = log;
+        }
+
+        public void SomeOperation()
+        {
+            int c = log.RecordCount;
+            log.LogInfo("Performing an operation");
+            if (c + 1 != log.RecordCount)
+                throw new Exception();
+            if (log.RecordCount >= log.RecordLimit)
+                throw new Exception();
+        }
+    }
+
+    public class NullLog : ILog
+    {
+        public int RecordLimit { get { return c + 1; } }
+
+        public int RecordCount 
+        { 
+            get 
+            {
+                var remainder = ++r % 3;
+
+                if (remainder == 1)
+                    return c + 1;
+                else
+                    return c;
+            }
+
+            set { c = value; } 
+        }
+        private int c = 0;
+        private int r = 2;
+
+        public void LogInfo(string message) { }
     }
 }
